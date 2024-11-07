fd5df1c885e7554feaae4fc3aa2a55f2d9d3e21e
DanteOnline <iamdanteonline@gmail.com>
2024-11-07 21:24:57 +0300

    more test coverage

--- testing/test_main.py
+++ testing/test_main.py
@@ -5,3 +5,7 @@ def test_sum():
 def test_hello(client):
     response = client.get('/')
     assert response.status_code == 200
+
+def test_about(client):
+    response = client.get('/about/some-str/')
+    assert response.status_code == 200
