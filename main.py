c498a2b3337254bd19d1e9dec7d53f0a0f38b675
DanteOnline <iamdanteonline@gmail.com>
2024-11-07 20:51:15 +0300

    fix linter

--- main.py
+++ main.py
@@ -15,15 +15,13 @@ def login():
         password = request.form['password']
         if login1 == 'admin' and password == '1234':
             return redirect(url_for('about', name=login1))
-        else:
-            return render_template('login.html')
-
+        return render_template('login.html')
     return render_template('login.html')
 
 
 @app.route('/about/<string:name>/')
 def about(name):
-    return (f'Привет, {name}! Эта страница о нашем курсе')
+    return f'Привет, {name}! Эта страница о нашем курсе'
 
 
 @app.route('/contact/')
@@ -44,13 +42,17 @@ def sum_numbers(num1, num2):
 
 
 @app.route('/len/<string:my_str>/')
-def len_str(my_str):
+def len_str(my_str):  # pylint:disable=missing-function-docstring
     result = len(my_str)
     return f'Длина строки {my_str} равна {result}'
 
 
 @app.route('/students/')
 def students():
+    """
+    Students view
+    :return:
+    """
     students1 = [
         {'name': 'Иван', 'course': 1, 'group': 101},
         {'name': 'Петр', 'course': 2, 'group': 201},
